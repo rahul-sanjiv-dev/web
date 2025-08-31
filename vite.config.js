fa84d4e073b73f3aa6ba8a3fba7a0fa3d79361a0
Rahul Sanjiv R <rahulsanjiv.r@gmail.com>
2025-08-31 15:59:20 +0530

    Clean repo: removed node_modules and added .gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web/',   // 👈 Must match your GitHub repo name
-  build: {
-    outDir: 'dist',   // Default output directory for production build
-  },
-  server: {
-    port: 5173,       // Local dev server port (optional)
-    open: true        // Opens browser automatically on dev
-  }
+  base: '/web/',
 })
